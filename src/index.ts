1a064694b22fc4282e002fba6e05a944c2dc0558
Fernando Menjivar <fernando@Johns-MacBook-Air.local>
2020-11-29 21:11:41 -0600

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import {ApplicationConfig, MantenimientoAppWsApplication} from './application';
-
 export * from './application';
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new MantenimientoAppWsApplication(options);
   await app.boot();
   await app.start();
-
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
@@ -18,6 +16,14 @@ if (require.main === module) {
   // Run the application
   const config = {
     rest: {
+      cors: {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
+      },
       port: +(process.env.PORT ?? 3000),
       host: process.env.HOST,
       // The `gracePeriodForClose` provides a graceful close for http/https
